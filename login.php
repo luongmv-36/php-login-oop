1ceecd7468751ba8bd84d7c2a59c37a4fad22458
Luong Mai Van <maivanluong1@gmail.com>
2018-03-23 09:21:08 +0700

    PHP Login With OOP

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'core/init.php';
+?>
+
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" autocomplete="off">
+    </div>
+    <div class="field">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" autocomplete="off">
+    </div>
+
+    <div class="field">
+        <label for="remember">
+            <input type="checkbox" name="remember" id="remember">Remember Me
+        </label>
+    </div>
+
+    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+    <input type="submit" value="Log In">
+</form>
